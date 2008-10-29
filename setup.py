cfe2c54bdb75423a7023ae37f7091da30b071119
Tom Gross <itconsense@gmail.com>
2008-10-29 12:27:19 +0000

    reverted last checking: svn merge 10191:10190 .
    svn path=/MoreFieldsAndWidgets/archetypes.referencebrowserwidget/trunk/; revision=10193

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0 (svn)'
+version = '0.9 (unreleased)'
 
 long_description = (
     read('README.txt')
