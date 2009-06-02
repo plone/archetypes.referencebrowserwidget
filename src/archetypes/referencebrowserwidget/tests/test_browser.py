c1735bb059e0c79d2a887a43752c7f81bcd1906d
Tom Lazar <tom@tomster.org>
2009-06-02 17:56:25 +0000

    Added a browser test that demonstrated the 'Insufficient Privileges' error raised by the widget template if the source references objects to which the editor doesn't have sufficient privileges.
    svn path=/MoreFieldsAndWidgets/archetypes.referencebrowserwidget/branches/jarn-unauthorized/; revision=11147

---
+++ src/archetypes/referencebrowserwidget/tests/test_browser.py
@@ -0,0 +1,16 @@
+from unittest import TestSuite
+from zope.testing import doctest
+from Testing import ZopeTestCase as ztc
+from archetypes.referencebrowserwidget.tests.base import FunctionalTestCase
+
+optionflags = (doctest.REPORT_ONLY_FIRST_FAILURE |
+               doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
+
+
+def test_suite():
+    return TestSuite([
+        ztc.FunctionalDocFileSuite('reference_access.txt',
+            package='archetypes.referencebrowserwidget.tests',
+            test_class=FunctionalTestCase,
+            optionflags=optionflags),
+    ])
