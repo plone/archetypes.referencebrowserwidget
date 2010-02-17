10ea4935a0683a34b398dbd6d3b94d9d2e62a76f
Tom Gross <itconsense@gmail.com>
2010-02-17 10:23:47 +0000

    back to development
    svn path=/MoreFieldsAndWidgets/archetypes.referencebrowserwidget/trunk/; revision=12241

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0b2'
+version = '1.0b4 (svn/unreleased)'
 
 long_description = (
     read('README.txt')
