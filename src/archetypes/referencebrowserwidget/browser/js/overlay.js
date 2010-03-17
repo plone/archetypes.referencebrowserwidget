d572bd9062844938eb1bd71f76abbcc1b6c82c9f
Tom Gross <itconsense@gmail.com>
2010-03-17 12:24:29 +0000

    rough cut of page-redirection in overlay
    svn path=/MoreFieldsAndWidgets/archetypes.referencebrowserwidget/branches/tom_gross-overlay/; revision=12341

--- src/archetypes/referencebrowserwidget/browser/js/overlay.js
+++ src/archetypes/referencebrowserwidget/browser/js/overlay.js
@@ -1,9 +1,38 @@
-jq(function(){
-   jq('input.addreference').prepOverlay(
-   {
+
+jq(function() {
+        /*
+   jq('input.addreference').prepOverlay({
         subtype: 'ajax',
         filter: '>*',
         closeselector: 'a.refbrowser_closewindow',
-   }
-);
+   });
+   
+   jQuery('.link').bind('click', function() {
+        :
+   });
+  */ 
+
+  jq('.addreference').overlay({
+       closeOnClick: false,
+       // TODO: enable close button
+       onBeforeLoad: function() {
+           var wrap = this.getContent().find('.overlaycontent');
+           // TODO: only select relevant parts of body
+           var src = this.getTrigger().attr('src');
+           wrap.load(src + ' >*');
+           }});
+
+  jq('[id^=atrb_] a').live('click', function(event) {
+      var src = jq(this).attr('href');
+      var wrap = jq(this).parents('.overlaycontent');
+      console.log(wrap);
+      wrap.load(src + ' #content');
+      return false;
+      });
+
+  //alert('0');
+
 });
+
+
+
