0b19cb11b351b012285e9315ac2c1d72464618bd
Tom Gross <itconsense@gmail.com>
2010-03-17 14:42:57 +0000

    fixed closing of window
    svn path=/MoreFieldsAndWidgets/archetypes.referencebrowserwidget/branches/tom_gross-overlay/; revision=12344

--- src/archetypes/referencebrowserwidget/browser/js/overlay.js
+++ src/archetypes/referencebrowserwidget/browser/js/overlay.js
@@ -18,6 +18,7 @@ jq(function() {
        onBeforeLoad: function() {
            var wrap = this.getContent().find('.overlaycontent');
            var src = this.getTrigger().attr('src');
+           wrap.data('overlay', this);
            wrap.load(src + ' >*');
            }});
 
@@ -34,9 +35,16 @@ jq(function() {
       var wrap = target.parents('.overlaycontent');
       var fieldname = wrap.find('input[name=fieldName]').attr('value');
       var multi = wrap.find('input[name=multiValued]').attr('value');
+      var close_window = wrap.find('input[name=close_window]').attr('value');
       var title = target.parents('tr').find('img').attr('alt');
       var uid = target.attr('rel');
       refbrowser_setReference(fieldname, uid, title, parseInt(multi));
+      if (close_window === '1') {
+          overlay = wrap.data('overlay');
+          overlay.close();
+      } else {
+          showMessage(title);
+      };
       return false;
 
           });
