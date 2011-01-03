1622559a833d18d22756d5608eac1ae85041d1bf
Eric Steele <esteele@plone.org>
2011-01-03 15:14:17 +0000

    Version bump.
    svn path=/MoreFieldsAndWidgets/archetypes.referencebrowserwidget/trunk/; revision=13387

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '2.1'
+version = '2.2'
 
 long_description = (
     read('README.txt')
