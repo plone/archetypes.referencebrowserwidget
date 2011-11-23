08445e40e6f6622faae8da88941aaa572633a8b2
Tom Gross <tom@toms-projekte.de>
2011-11-23 16:03:51 +0100

    Back to development: 2.4.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '2.4.8'
+version = '2.4.9dev'
 
 long_description = (
     read('README.txt')
