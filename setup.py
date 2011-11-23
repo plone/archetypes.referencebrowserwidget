7b240d23f8d65ec1a188b155204af52f5a6e323c
Tom Gross <tom@toms-projekte.de>
2011-11-23 15:20:34 +0100

    prepare release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '2.4.5dev'
+version = '2.4.5'
 
 long_description = (
     read('README.txt')
