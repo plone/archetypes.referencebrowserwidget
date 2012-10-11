8a030815911beb943cc4f9e9cdd2c9000cd76eb1
Eric Steele <EricSteele47@gmail.com>
2012-10-11 12:03:18 +0200

    Preparing release 2.4.13

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '2.4.13.dev2'
+version = '2.4.13'
 
 long_description = (
     read('README.txt')
