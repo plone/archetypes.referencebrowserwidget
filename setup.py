08374ba392983aa6bab112839ad8dfbfb1233539
Eric Steele <EricSteele47@gmail.com>
2012-12-14 23:30:57 -0500

    Back to development: 2.4.16

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '2.4.15'
+version = '2.4.16.dev0'
 
 long_description = (
     read('README.txt')
