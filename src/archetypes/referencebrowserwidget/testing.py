736a3b9d0a5b6489a75bf903b3d3b4f71065f738
Godefroid Chapelle <gotcha@bubblenet.be>
2014-03-12 16:57:04 +0100

    Get rid of PTC, first step

--- src/archetypes/referencebrowserwidget/testing.py
+++ src/archetypes/referencebrowserwidget/testing.py
@@ -1,7 +1,14 @@
+from Products.Archetypes.tests.utils import makeContent
+
 from plone.testing import z2
+from plone.testing import Layer
 
 from plone.app.testing import PloneWithPackageLayer
 from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import ploneSite
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
 
 from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
 
@@ -16,10 +23,60 @@ ATRB_SAMPLE_TYPES = PloneWithPackageLayer(
     name='ATRB_SAMPLE_TYPES',
 )
 
+
+class ReferenceBrowserWidgetWithData(Layer):
+
+    def testSetUp(self):
+        self.createDefaultStructure()
+
+    def testTearDown(self):
+        self.removeDefaultStructure()
+
+    def createDefaultStructure(self):
+        with ploneSite() as portal:
+            if 'layer1' not in portal.objectIds():
+                setRoles(portal, TEST_USER_ID, ['Manager'])
+                makeContent(portal, portal_type='Folder', id='layer1')
+                portal.layer1.setTitle('Layer1')
+                portal.layer1.reindexObject()
+                makeContent(portal.layer1, portal_type='Folder', id='layer2')
+                self.folder = portal.layer1.layer2
+                self.folder.setTitle('Layer2')
+                self.folder.reindexObject()
+                setRoles(portal, TEST_USER_ID, ['Member'])
+        self['layer2'] = portal.layer1.layer2
+
+    def removeDefaultStructure(self):
+        with ploneSite() as portal:
+            if 'layer1' in portal.objectIds():
+                portal._delObject('layer1')
+
+ATRB_WITH_DATA = ReferenceBrowserWidgetWithData(
+    bases=(
+        ATRB_SAMPLE_TYPES,
+    ),
+    name="ATRB_WITH_DATA",
+)
+
+ATRB_WITH_DATA_INTEGRATION = IntegrationTesting(
+    bases=(
+        ATRB_WITH_DATA,
+    ),
+    name="ATRB_WITH_DATA_INTEGRATION",
+)
+
+ATRB_WITH_DATA_FUNCTIONAL = FunctionalTesting(
+    bases=(
+        ATRB_WITH_DATA,
+    ),
+    name="ATRB_WITH_DATA_FUNCTIONAL",
+)
+
 ATRB_ROBOT_TESTING = FunctionalTesting(
     bases=(
         ATRB_SAMPLE_TYPES,
         REMOTE_LIBRARY_BUNDLE_FIXTURE,
         z2.ZSERVER_FIXTURE,
     ),
-    name="ATRB_ROBOT_TESTING")
+    name="ATRB_ROBOT_TESTING",
+)
--- src/archetypes/referencebrowserwidget/tests/test_utils.py
+++ src/archetypes/referencebrowserwidget/tests/test_utils.py
@@ -1,7 +1,9 @@
 import unittest
 
 from archetypes.referencebrowserwidget import utils
-from archetypes.referencebrowserwidget.tests.base import TestCase
+from archetypes.referencebrowserwidget.testing import (
+    ATRB_WITH_DATA_INTEGRATION
+)
 from Products.Archetypes.tests.utils import makeContent
 
 
@@ -20,10 +22,12 @@ class UtilsTestCase(unittest.TestCase):
         assert utils.quotequery('foo and or bar') == 'foo and "or" bar'
 
 
-class PloneUtilsTestCase(TestCase):
+class UtilsIntegrationTestCase(unittest.TestCase):
 
-    def afterSetUp(self):
-        self.folder = self.createDefaultStructure()
+    layer = ATRB_WITH_DATA_INTEGRATION
+
+    def setUp(self):
+        self.folder = self.layer['layer2']
         makeContent(self.folder, portal_type='Document', id='doc1')
         makeContent(self.folder, portal_type='Folder', id='folder1')
         makeContent(self.folder, portal_type='Folder', id='folder2')
@@ -33,31 +37,28 @@ class PloneUtilsTestCase(TestCase):
         self.doc1 = self.folder.doc1
         self.doc2 = self.folder.folder1.subfolder1.doc2
 
-    def beforeTearDown(self):
-        self.removeDefaultStructure()
-
     def test_emptystartupdir(self):
         self.assertEqual(
             utils.getStartupDirectory(self.doc1, ''),
-            'http://nohost/plone/layer1/layer2/doc1')
+            '/plone/layer1/layer2/doc1')
 
     def test_absstartupdir(self):
         self.assertEqual(
             utils.getStartupDirectory(self.doc1, '/rootfolder'),
-            'http://nohost/plone/rootfolder')
+            '/plone/rootfolder')
 
     def test_relstartupdir(self):
         self.assertEqual(
             utils.getStartupDirectory(self.doc2, '../../folder2'),
-            'http://nohost/plone/layer1/layer2/folder2')
+            '/plone/layer1/layer2/folder2')
 
         self.assertEqual(
             utils.getStartupDirectory(self.doc2, '..'),
-            'http://nohost/plone/layer1/layer2/folder1/subfolder1')
+            '/plone/layer1/layer2/folder1/subfolder1')
 
         self.assertEqual(
             utils.getStartupDirectory(self.doc2, '../..'),
-            'http://nohost/plone/layer1/layer2/folder1')
+            '/plone/layer1/layer2/folder1')
 
 
 def test_suite():
