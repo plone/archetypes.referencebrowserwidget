778dd514b467fea149d845b5d96e916941164a6f
Godefroid Chapelle <gotcha@bubblenet.be>
2014-03-13 11:52:47 +0100

    Back to development: 2.4.22

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '2.4.21'
+version = '2.4.22.dev0'
 
 long_description = (
     read('README.txt')
