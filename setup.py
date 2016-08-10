ff37bca1580728750ed1d6a97aef5439f813c265
Maurits van Rees <maurits@vanrees.org>
2016-08-10 23:26:34 +0200

    Preparing release 2.5.8
    [ci skip]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '2.5.8.dev0'
+version = '2.5.8'
 
 long_description = (
     read('README.rst')
